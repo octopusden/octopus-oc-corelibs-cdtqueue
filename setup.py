77225bdf3c2bfdaa709965cfa5a4c00c925cd1dd
Aleksey Knyazev <ctj_yebbs@inbox.ru>
2023-05-22 12:20:37 +0300

    Fixed "python_requires" for PyPI metadata compatibility (#8)

--- setup.py
+++ setup.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python
 from setuptools import setup
 from setuptools import find_packages
-from sys import version_info
 
 # NOTE: here we need tests also because many dependent packages uses them for their testing
 included_packages = find_packages()
 
-if version_info.major >= 3:
-    python_requires = ">=3.6"
-else:
-    python_requires = ">=2.7,<3"
-
-__version = "4.0.2"
+__version = "4.0.3"
 
 spec = {
         "name": "oc-cdt-queue2",
@@ -22,7 +16,7 @@ spec = {
         "license": "Apache2.0",
         "install_requires": ["pika==1.1.0"],
         "packages": included_packages,
-        "python_requires": python_requires
+        "python_requires": ">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*"
         }
 
 
