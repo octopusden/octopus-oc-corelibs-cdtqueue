20c32eea4b4aea3e0c19ca8526da0586ca571dd9
Ivan Anisimov <ivansa@me.com>
2024-01-16 17:55:43 +0200

    remove pika version, increase package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 # NOTE: here we need tests also because many dependent packages uses them for their testing
 included_packages = find_packages()
 
-__version = "4.0.3"
+__version = "4.1.0"
 
 spec = {
         "name": "oc-cdt-queue2",
@@ -14,7 +14,7 @@ spec = {
         "long_description": "",
         "long_description_content_type": "text/plain",
         "license": "Apache2.0",
-        "install_requires": ["pika==1.1.0"],
+        "install_requires": ["pika"],
         "packages": included_packages,
         "python_requires": ">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*"
         }
