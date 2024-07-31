e3d678b1cc579b462054db8b22f887d4112e7f25
ivansa-ru <ivansa@me.com>
2024-07-31 10:16:53 +0300

    remove python2 (#12)
    * remove python2
    
    * python version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 # NOTE: here we need tests also because many dependent packages uses them for their testing
 included_packages = find_packages()
 
-__version = "4.1.0"
+__version = "4.1.1"
 
 spec = {
         "name": "oc-cdt-queue2",
@@ -16,7 +16,7 @@ spec = {
         "license": "Apache2.0",
         "install_requires": ["pika"],
         "packages": included_packages,
-        "python_requires": ">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*"
+        "python_requires": ">=3.6"
         }
 
 
